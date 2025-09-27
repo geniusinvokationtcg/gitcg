5ee133fe65d87083f73086b56952f929993d7ed7
ersihfrans <jimjlf55@gmail.com>
2025-09-27 23:31:25 +0700

    fix discord permalink

--- app/[locale]/discord/page.tsx
+++ app/[locale]/discord/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
-export const discordPermalink = "https://discord.gg/VnsErdQA2y"
-
 export default function DiscordRedirect() {
   redirect(discordPermalink)
 }
+
+const discordPermalink = "https://discord.gg/VnsErdQA2y"
