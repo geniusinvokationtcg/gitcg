15d2ee403da24411bcebf364d41f9df6ef0efa17
ersihfrans <jimjlf55@gmail.com>
2026-01-05 23:40:48 +0700

    add vietnamese and normalize function for search bar

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,6 +1,6 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'id', 'zh-cn'],
+  locales: ['en', 'id', "vi", 'zh-cn'],
   defaultLocale: 'en'
 });
--- utils/formatting.ts
+++ utils/formatting.ts
@@ -1,7 +1,17 @@
+import { Locales } from "./types";
+
 export function percentize(n: number | string, locale: string): string{
   if(typeof n === "string") n = parseFloat(n);
   return `${(n*100).toLocaleString(locale,{
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   })}%`
+}
+
+export function normalizeSearchText (text: string, locale: Locales): string {
+  return text
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d");
 }
