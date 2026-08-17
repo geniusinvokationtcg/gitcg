8477b1bace7f3d680719cd673c39b4126e57eb99
ersihfrans <jimjlf55@gmail.com>
2026-08-17 22:10:09 +0700

    7.0 week 1

--- app/[locale]/leaderboard/seasons.ts
+++ app/[locale]/leaderboard/seasons.ts
@@ -3,6 +3,12 @@ import { Season, Seasons } from "@/utils/types"
 export const seasons: Seasons = {
   na: [
     {
+      versions: ["7-0", "7-1", "7-2"],
+      best_finish: 3,
+      qualification_type: "top",
+      qualification_threshold: 8
+    },
+    {
       versions: ["6-5", "6-6", "6-7"],
       best_finish: 3,
       qualification_type: "top",
